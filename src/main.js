dac24c28c7ec03ba1d1568dfa4f3718ddce17983
daimiao1005 <1042337903@qq.com>
2019-08-31 19:48:45 +0800

    黑马头条PC-初始化项目-页面分析-新建登录和主页

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui' // 引入UI
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import './style/index.less'
 Vue.config.productionTip = false
-
+Vue.use(ElementUI) // 全局注册elementUI组件 任意位置都可以使用
 new Vue({
   router,
   render: h => h(App)
